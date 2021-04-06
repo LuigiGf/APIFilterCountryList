15d79b7b059e0d570d27eb3a3dcb44106fa00bf2
LuigiGF <luigi.gottardello.fonseca@gmail.com>
2021-04-05 22:15:00 -0300

    Finishing the style for react app filter countries

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ export default class App extends Component {
 
     return (
       <div className="container">
-        <h1>React Countries</h1>
+        <h1 style={styles.centeredTitle}>React Countries</h1>
         <Header
           filter={filter}
           countryCount={filteredCountries.length}
@@ -82,3 +82,9 @@ export default class App extends Component {
     );
   }
 }
+
+const styles = {
+  centeredTitle: {
+    textAlign: "center",
+  },
+};
