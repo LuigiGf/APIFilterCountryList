3cb44f46b26d0f5fb96621f0e845bcbbfe75bcba
LuigiGF <luigi.gottardello.fonseca@gmail.com>
2021-04-05 21:34:58 -0300

    Finishing filtered function header

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ export default class App extends Component {
     this.state = {
       allCountries: [],
       filteredCountries: [],
+      filteredPopulation: 0,
       filter: "",
     };
   }
@@ -21,31 +22,62 @@ export default class App extends Component {
       return {
         id: numericCode,
         name,
+        filterName: name.toLowerCase(),
         flag,
         population,
       };
     });
 
+    const filteredPopulation = this.calculateTotalPopulationFrom(allCountries);
+
     this.setState({
       allCountries,
-      filteredCountries: allCountries,
+      filteredCountries: Object.assign([], allCountries),
+      filteredPopulation,
     });
   }
 
-  handleChangeFilter = (newFilter) => {
+  calculateTotalPopulationFrom = (countries) => {
+    const totalPopulation = countries.reduce((accumulator, current) => {
+      return accumulator + current.population;
+    }, 0);
+    return totalPopulation;
+  };
+
+  handleChangeFilter = (newText) => {
     this.setState({
-      filter: newFilter,
+      filter: newText,
+    });
+
+    const filterLowerCase = newText.toLowerCase();
+
+    const filteredCountries = this.state.allCountries.filter((country) => {
+      return country.filterName.includes(filterLowerCase);
+    });
+
+    const filteredPopulation = this.calculateTotalPopulationFrom(
+      filteredCountries
+    );
+
+    this.setState({
+      filteredCountries,
+      filteredPopulation,
     });
   };
 
   render() {
-    const { allCountries, filter } = this.state;
+    const { filteredCountries, filter, filteredPopulation } = this.state;
 
     return (
       <div className="container">
         <h1>React Countries</h1>
-        <Header filter={filter} onChangeFilter={this.handleChangeFilter} />
-        <Countries countries={allCountries} />
+        <Header
+          filter={filter}
+          countryCount={filteredCountries.length}
+          totalPopulation={filteredPopulation}
+          onChangeFilter={this.handleChangeFilter}
+        />
+        <Countries countries={filteredCountries} />
       </div>
     );
   }
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -6,11 +6,12 @@ export default class Header extends Component {
     this.props.onChangeFilter(newText);
   };
   render() {
-    const { filter } = this.props;
+    const { filter, countryCount, totalPopulation } = this.props;
     return (
       <div>
         <input type="text" value={filter} onChange={this.handleInputChange} /> |
-        <span>Total de países: </span> |<span>População: </span>
+        <span>Países: {countryCount}</span> |
+        <span>População: {totalPopulation}</span>
       </div>
     );
   }
